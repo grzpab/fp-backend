c43fb2a47d135a68f3d9689d4197360defef4e3f
grzpab <grzpab@users.noreply.github.com>
2020-11-14 19:13:16 +0100

    remove unused codec

--- src/codecs/sharedCodecs.ts
+++ src/codecs/sharedCodecs.ts
@@ -4,9 +4,6 @@ import { failure } from "io-ts/PathReporter";
 export const buildRetCodec = <T extends t.Props>(props: T): t.ReadonlyC<t.ExactC<t.TypeC<T>>> =>
     t.readonly(t.exact(t.type(props)));
 
-export const buildRepCodec = <T extends t.Props>(props: T): t.ReadonlyC<t.ExactC<t.PartialC<T>>> =>
-    t.readonly(t.exact(t.partial(props)));
-
 export const emptyCodec = buildRetCodec({});
 
 export const mapErrors = (errors: t.Errors): string => failure(errors).join(",");
